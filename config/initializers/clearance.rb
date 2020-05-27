0991af15b24af9cd8ed34f094cbacc72673515ed
Ryan Richardson <ryananthonyrichardson@gmail.com>
2020-05-27 01:07:40 -0700

    Initial Commit

---
+++ config/initializers/clearance.rb
@@ -0,0 +1,23 @@
+Clearance.configure do |config|
+  config.routes = false
+  config.mailer_sender = "noreply@freednd.com"
+  config.rotate_csrf_on_sign_in = true
+
+  ########################
+  ## Clearance Defaults ##
+  ########################
+  #config.allow_sign_up = true
+  #config.cookie_expiration = lambda { |cookies| 1.year.from_now.utc }
+  #config.cookie_name = "remember_token"
+  # this one will break things in set and unmatched...
+  #config.cookie_domain = '.example.com'
+  #config.cookie_path = "/"
+  #config.routes = true
+  #config.httponly = false
+  #config.password_strategy = Clearance::PasswordStrategies::BCrypt
+  #config.redirect_url = "/"
+  #config.secure_cookie = false
+  #config.sign_in_guards = []
+  #config.user_model = "User"
+  #config.parent_controller = "ApplicationController"
+end
---
+++ config/routes.rb
@@ -0,0 +1,19 @@
+Rails.application.routes.draw do
+  root 'landing_pages#index'
+
+  ###################
+  ## User Sessions ##
+  ###################
+  resources :passwords, controller: "clearance/passwords", only: [:create, :new]
+  resource :session, controller: "clearance/sessions", only: [:create]
+
+  resources :users, controller: "clearance/users", only: [:create] do
+    resource :password,
+      controller: "clearance/passwords",
+      only: [:edit, :update]
+  end
+
+  get "/sign_in" => "clearance/sessions#new", as: "sign_in"
+  delete "/sign_out" => "clearance/sessions#destroy", as: "sign_out"
+  get "/sign_up" => "clearance/users#new", as: "sign_up"
+end
