3d7f4dd8af0dc36091a46fd3b3f00cbe0e5e610b
Ryan Richardson <ryananthonyrichardson@gmail.com>
2020-05-29 01:58:24 -0700

    Persist DnD5eAPI.co spell and feature data as seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,58 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+seed_files = []
+if File.exist?("#{Rails.root}/db/seeds/dnd5eapi_co_spells.rb")
+  seed_files << "#{Rails.root}/db/seeds/dnd5eapi_co_spells.rb"
+else
+  data = query_dnd5eapi('/api/spells')
+  spells_data = []
+  data['results'].each do |hash|
+    meta = query_dnd5eapi(hash['url'])
+    puts "Creating Spell: #{meta['name']}"
+    Spell.create! meta.
+      slice(*Spell.new.attributes.keys).
+      merge(
+        'slug' => meta['index'],
+        'school' => meta['school']['name'],
+        'description' => meta['desc'].map(&:inspect).join(', '),
+        'dnd_classes' => meta['classes'].map{|c| c['name']}.join(', '),
+        'level_conditions' => meta['higher_level']&.
+          map(&:inspect)&.join(', '),
+        'requires_concentration' => meta['concentration'],
+        'components' => meta['components']&.map(&:inspect)&.join(', '),
+        'is_ritual' => meta['ritual']
+      )
+    sleep 0.2
+  end
+end
+
+if File.exist?("#{Rails.root}/db/seeds/dnd5eapi_co_features.rb")
+  seed_files << "#{Rails.root}/db/seeds/dnd5eapi_co_features.rb"
+else
+  data = query_dnd5eapi('/api/features')
+  spells_data = []
+  data['results'].each do |hash|
+    meta = query_dnd5eapi(hash['url'])
+    puts "Creating Feature: #{meta['name']}"
+    Feature.create!({
+      'name' => meta['name'],
+      'level' => meta['level'],
+      'dnd_class' => meta['class']['name'],
+      'description' => meta['desc'].map(&:inspect).join(', ')
+    })
+    # prevent hammering
+    sleep 0.2
+  end
+end
+
+if seed_files.any?
+  seed_files.each do |path_to_seed_file|
+    puts "Seeding #{path_to_seed_file}"
+    load(path_to_seed_file)
+  end
+end
+
 users = []
 (1..3).to_a.map do |n|
   users << User.create!(email: "user#{n}@freednd.com", password: 'pw')
@@ -23,7 +75,7 @@ puts 'Created Campaign'
 
 shared_attributes = {
   dnd_class: 'Bard',
-  race: 'Druid',
+  race: 'Human',
   background: 'Outlander',
   alignment: 'Lawful Good'
 }
@@ -41,3 +93,13 @@ users.map do |user|
 end
 puts "Created #{chars.count} Characters in Party"
 
+# Utility function for getting data from https://www.dnd5eapi.co/
+def query_dnd5eapi(endpoint)
+  url = URI('https://www.dnd5eapi.co' + endpoint)
+  http = Net::HTTP.new(url.host, url.port)
+  http.use_ssl = true
+  http.verify_mode = OpenSSL::SSL::VERIFY_PEER
+  api_request = Net::HTTP::Get.new(url)
+  api_response = http.request(api_request)
+  JSON.parse(api_response&.body)
+end
