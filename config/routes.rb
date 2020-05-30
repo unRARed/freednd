2b4f578bc245b4a091a5585c15222a9a0aa7bc43
Ryan Richardson <ryananthonyrichardson@gmail.com>
2020-05-30 12:08:14 -0700

    Update forms

--- config/routes.rb
+++ config/routes.rb
@@ -6,13 +6,18 @@ Rails.application.routes.draw do
   ###################
   ## User Sessions ##
   ###################
-  resources :passwords, controller: "clearance/passwords", only: [:create, :new]
-  resource :session, controller: "clearance/sessions", only: [:create]
+  resources :passwords,
+    controller: "clearance/passwords", only: [:create, :new]
+  resource :session,
+    controller: "clearance/sessions", only: [:new, :create]
+  get 'session' => redirect('sign_in')
 
-  resources :users, controller: "clearance/users", only: [:create] do
+  resources(:users,
+    controller: "clearance/users",
+    only: [:new, :create]
+  ) do
     resource :password,
-      controller: "clearance/passwords",
-      only: [:edit, :update]
+      controller: "clearance/passwords", only: [:edit, :update]
   end
 
   get "/sign_in" => "clearance/sessions#new", as: "sign_in"
