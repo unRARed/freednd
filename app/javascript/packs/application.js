a976d570a942acb6bcf8d91bdc96ec0619840160
Ryan Richardson <ryananthonyrichardson@gmail.com>
2020-05-30 00:59:22 -0700

    Add tachyons for CSS utilities

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,11 +8,8 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
-function importAll (r) {
-  r.keys().forEach(r);
-}
-
-importAll(require.context('../modules/', true, /\.js$/));
+function importAll (r) { r.keys().forEach(r) }
+importAll(require.context('../modules/', true, /\.js$/))
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
