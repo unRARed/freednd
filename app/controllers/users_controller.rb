32e279c0c748fa62797ee4bfdaa4282149714448
Ryan Richardson <ryananthonyrichardson@gmail.com>
2020-07-22 19:53:55 -0700

    Make campaign#show less crappy

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,5 +8,8 @@ class UsersController < Clearance::UsersController
 
   def account
     authorize(User)
+    return redirect_to root_path unless current_user
+    @campaigns = current_user.campaigns +
+      current_user.character_campaigns
   end
 end
