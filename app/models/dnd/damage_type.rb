bc9af88d05ff8c9d96c53584ffc957f78b9b4716
Ryan Richardson <ryananthonyrichardson@gmail.com>
2020-09-03 01:45:24 -0700

    Show damage info for weapons

--- app/models/dnd/damage_type.rb
+++ app/models/dnd/damage_type.rb
@@ -1,2 +1,4 @@
 class DnD::DamageType < DnD::Entity
+  has_one :dnd_weapon,
+    class_name: 'DnD::Weapon'
 end
--- app/models/dnd/weapon.rb
+++ app/models/dnd/weapon.rb
@@ -1,2 +1,6 @@
 class DnD::Weapon < DnD::Equipment
+  belongs_to :damage_type,
+    optional: true,
+    foreign_key: :dnd_weapon_damage_type_id,
+    class_name: '::DnD::DamageType'
 end
