67d21e38d548c399c1f9188c4daee6fdd4101b04
Ryan Richardson <ryananthonyrichardson@gmail.com>
2020-10-15 23:32:30 -0700

    Sort and limit feature selection by dnd class, closes #58

--- app/models/dnd/feature.rb
+++ app/models/dnd/feature.rb
@@ -1,2 +1,7 @@
 class DnD::Feature < ApplicationRecord
+  scope :by_dnd_class,-> (dnd_class) {
+    where(dnd_class_name: dnd_class.downcase).
+    order('dnd_class_name ASC')
+  }
+  scope :by_level, -> { order('level ASC') }
 end
